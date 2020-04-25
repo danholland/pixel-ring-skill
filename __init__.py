ce75ca11f1b00df921e9bf6598dfc8fcadc77908
danholland <danieljholland@gmail.com>
2020-04-25 21:26:24 +0100

    self self self self

--- __init__.py
+++ __init__.py
@@ -6,37 +6,38 @@ import os
 import time
 from .pixel_ring import PixelRing
 
+
 class PixelRingSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        en = mraa.Gpio(12)
-        if os.geteuid() != 0 :
+        self.en = mraa.Gpio(12)
+        if os.geteuid() != 0:
             time.sleep(1)
-    
-        en.dir(mraa.DIR_OUT)
-        en.write(0)
 
-        pixel_ring = PixelRing()
-    
+        self.en.dir(mraa.DIR_OUT)
+        self.en.write(0)
+
+        self.pixel_ring = PixelRing()
+
     def initialize(self):
         self.log.info("Pixel Ring initializing")
-        pixel_ring.set_brightness(20)
-        pixel_ring.wakeup()
+        self.pixel_ring.set_brightness(20)
+        self.pixel_ring.wakeup()
 
     @intent_file_handler('ring.pixel.intent')
     def handle_ring_pixel(self, message):
         self.speak_dialog('ring.pixel')
-        pixel_ring.wakeup()
+        self.pixel_ring.wakeup()
 
     def stop(self):
         self.log.debug("Pixel Ring stopping")
-        pixel_ring.off()
+        self.pixel_ring.off()
 
     def shutdown(self):
         self.log.debug("Pixel Ring shutting down")
-        pixel_ring.off()
-        en.write(1)
+        self.pixel_ring.off()
+        self.en.write(1)
+
 
 def create_skill():
     return PixelRingSkill()
-
