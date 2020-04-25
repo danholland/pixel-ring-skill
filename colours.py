64eb8ca9bb4743f0a8eb34b9a2c93a03afcccb7f
danholland <danieljholland@gmail.com>
2020-04-25 17:59:54 +0100

    Colours

---
+++ colours.py
@@ -0,0 +1,26 @@
+Colours = {
+    'light_blue': 0x5db3ce,
+    'blue': 0x0000ff,
+    'aquamarine': 0x5fbebe,
+    'purple': 0x985fbe
+}
+
+def fade(start, end, steps = 6):
+    s = []
+    f = []
+    s.append((start & 0xFF0000) >> 16)
+    s.append((start & 0x00FF00) >> 8)
+    s.append((start & 0x0000FF))
+    f.append((end & 0xFF0000) >> 16)
+    f.append((end & 0x00FF00) >> 8)
+    f.append((end & 0x0000FF))
+    arr_result = []
+    for i in range(steps):
+        curr_vector = [
+            int(s[j] + (float(i)/(steps-1))*(f[j]-s[j]))
+            for j in range(3)
+        ]
+        res = "0x"+"".join(["0{0:x}".format(v) if v < 16 else
+            "{0:x}".format(v) for v in curr_vector])
+        arr_result.append(int(res, 16))
+    return arr_result
