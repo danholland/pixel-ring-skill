b16e33edccfe955197a0d65677a6dba491c6ee68
danholland <danieljholland@gmail.com>
2020-04-25 20:27:46 +0100

    Logging

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.messagebus.message import Message
-from mycroft.util.log import LOG
 
 import mraa
 import os
@@ -10,9 +9,6 @@ from .pixel_ring import PixelRing
 class PixelRingSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-    
-    def initialize(self):
-        LOG.debug("Pixel Ring initializing")
         en = mraa.Gpio(12)
         if os.geteuid() != 0 :
             time.sleep(1)
@@ -21,16 +17,23 @@ class PixelRingSkill(MycroftSkill):
         en.write(0)
 
         pixel_ring = PixelRing()
-
+    
+    def initialize(self):
+        self.log.info("Pixel Ring initializing")
         pixel_ring.set_brightness(20)
         pixel_ring.wakeup()
 
     @intent_file_handler('ring.pixel.intent')
     def handle_ring_pixel(self, message):
         self.speak_dialog('ring.pixel')
+        pixel_ring.wakeup()
 
     def stop(self):
-        LOG.debug("Pixel Ring stopping")
+        self.log.debug("Pixel Ring stopping")
+        pixel_ring.off()
+
+    def shutdown(self):
+        self.log.debug("Pixel Ring shutting down")
         pixel_ring.off()
         en.write(1)
 
