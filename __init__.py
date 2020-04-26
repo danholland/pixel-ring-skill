98acfada58ad50a50623567424f1d79b45e15e18
danholland <danieljholland@gmail.com>
2020-04-26 17:41:02 +0100

    I hate python

--- __init__.py
+++ __init__.py
@@ -21,6 +21,8 @@ class PixelRingSkill(MycroftSkill):
 
     def initialize(self):
         self.log.info("PixelRing initializing")
+        self.register_intent_file(
+            'ring.pixel.demo.intent', self.handle_ring_pixel_demo)
         brightness = self.settings.get('brightness', 15)
         self.pixel_ring.set_brightness(brightness)
         self.pixel_ring.wakeup()
@@ -31,12 +33,11 @@ class PixelRingSkill(MycroftSkill):
         self.pixel_ring.set_brightness(brightness)
         self.pixel_ring.change_pattern(pattern)
 
-    @intent_handler('ring.pixel.intent')
+    @intent_file_handler('ring.pixel.intent')
     def handle_ring_pixel(self, message):
         self.speak_dialog('ring.pixel')
         self.pixel_ring.wakeup()
 
-    @intent_handler('ring.pixel.demo.intent')
     def handle_ring_pixel_demo(self, message):
         self.log.info("Running Pixel Ring demo")
         self.pixel_ring.wakeup()
