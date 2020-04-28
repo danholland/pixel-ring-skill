1dda0909f1e41543c78948b578482c3b4662b8dc
danholland <danieljholland@gmail.com>
2020-04-28 16:01:37 +0100

    Change pattern intent

--- __init__.py
+++ __init__.py
@@ -35,19 +35,59 @@ class PixelRingSkill(MycroftSkill):
         self.speak_dialog('ring.pixel')
         self.pixel_ring.wakeup()
 
+    @intent_file_handler('ring.pixel.change.intent')
+    def handle_ring_pixel_change(self, message):
+        pattern_type = message.data.get('type')
+        self.log.info(pattern_type)
+
+        def google():
+            self.log.info("GOOGLE")
+            self.pixel_ring.change_pattern('google')
+
+        def echo():
+            self.log.info("ECHO")
+            self.pixel_ring.change_pattern('echo')
+
+        def on():
+            self.log.info("ON")
+            self.pixel_ring.off()
+            self.en.write(0)
+            self.pixel_ring.wakeup()
+
+        def off():
+            self.log.info("OFF")
+            self.stop()
+
+        def unknown():
+            self.log.info("UNKNOWN")
+            self.speak("Sorry, I don't understand")
+        known_types = {
+            google: google,
+            echo: echo,
+            amazon: echo,
+            on: on,
+            off: off
+        }
+        func = known_types.get(pattern_type, "unknown")
+        return func()
+
+
     @intent_file_handler('ring.pixel.demo.intent')
     def handle_ring_pixel_demo(self, message):
         self.log.info("Running Pixel Ring demo")
-        self.log.info("PixelRing Wakeup")
+        self.log.debug("PixelRing Wakeup")
         self.pixel_ring.wakeup()
         time.sleep(3)
-        self.log.info("PixelRing Think")
+        self.log.debug("PixelRing Listen")
+        self.pixel_ring.listen()
+        time.sleep(3)
+        self.log.debug("PixelRing Think")
         self.pixel_ring.think()
         time.sleep(3)
-        self.log.info("PixelRing Speak")
+        self.log.debug("PixelRing Speak")
         self.pixel_ring.speak()
         time.sleep(3)
-        self.log.info("PixelRing Off")
+        self.log.debug("PixelRing Off")
         self.pixel_ring.off()
 
     def stop(self):
